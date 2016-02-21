5c4bef9190633e65e8ec7fb90b4a1cb1bb00d5a5
Serghei Iakovlev <sadhooklay@gmail.com>
2016-02-21 17:59:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Phalcon Breadcrumbs
+
+Phalcon Breadcrumbs is a powerful and flexible component for building site breadcrumbs.
+You can adapt it to your own needs or improve it if you want.
+
+Please write us if you have any feedback.
+
+Thanks!
+
+## NOTE
+
+The master branch will always contain the latest stable version. If you wish
+to check older versions or newer ones currently under development, please
+switch to the relevant branch/tag.
+
+## Get Started
+
+### Requirements
+
+To use this component, you need at least:
+
+* [Composer][:composer:]
+* PHP >= 5.4
+* Latest stable [Phalcon Framework release][:phalcon:] extension enabled
+
+### Installing
+
+Install composer in a common location or in your project:
+
+```sh
+$ curl -s http://getcomposer.org/installer | php
+```
+
+Create the composer.json file as follows:
+
+```json
+{
+    "require": {
+        "phalcongelist/breadcrumbs": "dev-master"
+    }
+}
+```
+
+Run the composer installer:
+
+```sh
+$ php composer.phar install
+```
+
+### Usage
+
+We recommend registering it with your application's services for even easier use:
+
+```php
+use Phalcon\Breadcrumbs;
+
+// Initialize the Breadcrumbs component.
+$di->setShared('breadcrumbs', function () {
+    return new Breadcrumbs;
+});
+```
+
+ #### Adding a crumb with a link
+
+ ```php
+ $this->breadcrumbs->add('/', 'Home');
+ ```
+
+ #### Adding a crumb without a link (Normally the last one)
+
+ ```php
+ $this->breadcrumbs->add(null, 'User', false);
+ ```
+
+ #### Render crumbs
+
+  ```php
+  $this->breadcrumbs->render();
+  ```
+
+## Copyright
+
+Phalcon Breadcrumbs is open-sourced software licensed under the [New BSD License][:license:].
+© Phalcon Framework Team and contributors
+
+[:composer:]: https://getcomposer.org/
+[:phalcon:]: https://github.com/phalcon/cphalcon/releases
+[:license:]: https://github.com/phalcongelist/breadcrumbs/blob/master/docs/LICENSE.md
---
+++ src/Breadcrumbs.php
@@ -0,0 +1,276 @@
+<?php
+
+/*
+ +------------------------------------------------------------------------+
+ | Phalcon Breadcrumbs                                                    |
+ +------------------------------------------------------------------------+
+ | Copyright (c) 2016 Phalcon Framework Team                              |
+ +------------------------------------------------------------------------+
+ | This source file is subject to the New BSD License that is bundled     |
+ | with this package in the file docs/LICENSE.txt.                        |
+ |                                                                        |
+ | If you did not receive a copy of the license and are unable to         |
+ | obtain it through the world-wide-web, please send an email             |
+ | to serghei@phalconphp.com so I can send you a copy immediately.        |
+ +------------------------------------------------------------------------+
+ | Authors: Serghei Iakovlev <serghei@phalconphp.com>                     |
+ +------------------------------------------------------------------------+
+*/
+
+namespace Phalcon;
+
+use Phalcon\DiInterface;
+use Phalcon\Mvc\User\Component;
+use Phalcon\Events\ManagerInterface;
+
+/**
+ * \Phalcon\Breadcrumbs
+ *
+ * Handles the breadcrumbs for the application.
+ *
+ * @package Phalcon
+ */
+class Breadcrumbs extends Component
+{
+    /**
+     * Keeps all the breadcrumbs
+     * @var array
+     */
+    protected $elements = [];
+
+    /**
+     * Internal logger
+     * @var null|Logger\AdapterInterface
+     */
+    protected $logger;
+
+    /**
+     * Crumb separator
+     * @var string
+     */
+    protected $separator = ' / ';
+
+    /**
+     * Array holding output template
+     * @var array
+     */
+    protected $template = [
+        'linked'     => '<li><a href="{{link}}">{{icon}}{{label}}</a></li>',
+        'not-linked' => '<li class="active">{{icon}}{{label}}</li>',
+        'icon'       => '<i class="fa fa-dashboard"></i>'
+    ];
+
+    /**
+     * Internal translate adapter
+     * @var null|Translate\AdapterInterface
+     */
+    protected $translate;
+
+    public function __construct()
+    {
+        if ($this->getDI()->has('logger')) {
+            $logger = $this->getDI()->getShared('logger');
+            if ($logger instanceof Logger\AdapterInterface) {
+                $this->logger = $logger;
+            }
+        }
+
+        if ($this->getDI()->has('translate')) {
+            $translate = $this->getDI()->getShared('translate');
+            if ($translate instanceof Translate\AdapterInterface) {
+                $this->translate = $translate;
+            }
+        }
+
+        if ($this->getDI()->has('eventsManager')) {
+            $this->setEventsManager($this->getDI()->getShared('eventsManager'));
+        }
+    }
+
+    /**
+     * Gets internal translate adapter
+     *
+     * @return null|Translate\AdapterInterface
+     */
+    public function getTranslateAdapter()
+    {
+        return $this->translate;
+    }
+
+    /**
+     * Sets internal translate adapter
+     *
+     * @param Translate\AdapterInterface $translate Translate adapter
+     * @return $this
+     */
+    public function setTranslateAdapter(Translate\AdapterInterface $translate)
+    {
+        $this->translate = $translate;
+
+        return $this;
+    }
+
+    /**
+     * Gets internal logger
+     *
+     * @return null|Logger\AdapterInterface
+     */
+    public function getLogger()
+    {
+        return $this->logger;
+    }
+
+    /**
+     * Sets internal logger
+     *
+     * @param Logger\AdapterInterface $logger
+     */
+    public function setLogger(Logger\AdapterInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Gets crumb separator
+     *
+     * @return string
+     */
+    public function getSeparator()
+    {
+        return $this->separator;
+    }
+
+    /**
+     * Sets crumb separator
+     *
+     * @param string $separator Separator
+     * @return $this
+     */
+    public function setSeparator($separator)
+    {
+        try {
+            if (!is_string($separator)) {
+                $type = gettype($separator);
+                throw new \InvalidArgumentException(
+                    "Expected value of separator to be string, {$type} given."
+                );
+            }
+
+            $this->separator = $separator;
+        } catch (\Exception $e) {
+            $this->log($e->getMessage());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Adds a new crumb
+     *
+     * @param string $link The link that will be used
+     * @param string $label Text displayed in the breadcrumb trail
+     * @param bool $linked If false no link will be returned when rendering
+     * @return $this
+     */
+    public function add($link, $label, $linked = true)
+    {
+        try {
+            $id = md5(json_encode([$link, $label, $linked]));
+
+            if (!is_string($link) && !is_null($link)) {
+                $type = gettype($link);
+                throw new \InvalidArgumentException(
+                    "Expected value of second argument to be either string or null, {$type} given."
+                );
+            }
+
+            if (!is_string($label)) {
+                $type = gettype($label);
+                throw new \InvalidArgumentException(
+                    "Expected value of third argument to be string, {$type} given."
+                );
+            }
+
+            $this->elements[$id] = [
+                'link' => $link,
+                'label' => $label,
+                'linked' => (bool) $linked
+            ];
+        } catch (\Exception $e) {
+            $this->log($e->getMessage());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Render breadcrumb output based on previously set template
+     */
+    public function render()
+    {
+        if (empty($this->elements)) {
+            return;
+        }
+
+        $output = '';
+        $i = 0;
+        foreach ($this->elements as $key => $crumb) {
+            $label = $crumb['label'];
+            if ($this->translate) {
+                $label = $this->translate->query($label);
+            }
+
+            if ($crumb['linked']) {
+                $output .= str_replace(
+                    ['{{link}}', '{{label}}'],
+                    [$crumb['link'], $label],
+                    $this->template['linked']
+                );
+            } else {
+                $output .= str_replace('{{label}}', $label, $this->template['not-linked']);
+            }
+
+            if (1 == $i) {
+                $output = str_replace('{{icon}}', $this->template['icon'], $output);
+            }
+
+            $this->remove($key);
+            $output .= (!empty($this->elements) ? $this->separator : '');
+        }
+
+        echo $output;
+    }
+
+    /**
+     * Removes crumb by id
+     *
+     * @param string $id Crumb ID
+     * @return $this
+     */
+    public function remove($id)
+    {
+        try {
+            if (!is_scalar($id)) {
+                $type = gettype($id);
+                throw new \InvalidArgumentException("Expected value of first argument to be string, {$type} given.");
+            }
+
+            if (!empty($this->elements) && array_key_exists($id, $this->elements)) {
+                unset($this->elements[$id]);
+            }
+        } catch (\Exception $e) {
+            $this->log($e->getMessage());
+        }
+
+        return $this;
+    }
+
+    protected function log($message)
+    {
+        if ($this->logger) {
+            $this->logger->error($message);
+        } else {
+            error_log($message);
+        }
+    }
+}
